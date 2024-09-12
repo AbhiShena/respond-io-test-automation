1011feb77b8c8cd54b4379e14729bb5dcd2c5c74
AbhiShena <57492261+AbhiShena@users.noreply.github.com>
2024-09-12 08:53:26 +0800

    Update LoginApiTest.java

--- respond-io-test-automation/src/api/LoginApiTest.java
+++ respond-io-test-automation/src/api/LoginApiTest.java
@@ -1,1 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginApiTest {
+    @Test
+    public void testValidLoginApi() {
+        RestAssured.baseURI = "https://api.respond.io";
+        Response response = RestAssured
+            .given()
+            .header("Content-Type", "application/json")
+            .body("{\"email\": \"testuser@example.com\", \"password\": \"password123\"}")
+            .when()
+            .post("/user/login");
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        // Additional assertions can be made here
+    }
+}
 
